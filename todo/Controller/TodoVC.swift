babb67f57d527658a9b2fd93d53e433cd69b4b15
memo figueredo <memof80@gmail.com>
2020-12-30 14:07:45 -0500

    add to data provider to api and this onSucees configurating

--- todo/Controller/TodoVC.swift
+++ todo/Controller/TodoVC.swift
@@ -16,15 +16,33 @@ class TodoVC: UIViewController {
     
     @IBOutlet weak var prioritySegment: UISegmentedControl!
     
+    @IBOutlet weak var todoTable: UITableView!
+    
+//    MARK: variables
+    var todos = Array<Todo>()
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //call protocol before call data to api first see table before see data
+        todoTable.delegate = self
+        todoTable.dataSource = self
+        // function to solicitud newtwork api
+        getTodos()
+    }
+    
+    
+//    MARK: Fucntions
+    func getTodos(){
         NetworkService.shared.getTodos { (todos) in
             debugPrint(todos)
+            self.todos = todos.items
+            self.todoTable.reloadData()
         }
     }
+    
     //    MARK: IBAction
     @IBAction func addTodo(_ sender: Any) {
         
@@ -32,3 +50,28 @@ class TodoVC: UIViewController {
     
 }
 
+extension TodoVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell") as? TodoCell else {
+            return UITableViewCell()
+        }
+        cell.updateCell(todo: todos[indexPath.row])
+        return cell
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell") as? TodoCell {
+//
+//        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+}
--- todo/View/TodoCell.swift
+++ todo/View/TodoCell.swift
@@ -13,15 +13,18 @@ class TodoCell: UITableViewCell {
     
     @IBOutlet weak var priorityView: UIView!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    func updateCell(todo: Todo) {
+        itemLbl.text = todo.item
+        
+        switch todo.priority {
+        case 0:
+            priorityView.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+            break
+        case 2:
+            priorityView.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+            break
+        default:
+            priorityView.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
